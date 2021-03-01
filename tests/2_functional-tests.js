ba0e702bec3a0661cfc43e20759ee6f0b324561e
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-01 13:06:44 -0800

    Functional tests for /api/check: placement failures

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,6 +10,12 @@ const invalidChars = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47..
 const invalidLength = validUnsolved + '1';
 
 suite('Functional Tests', function () {
+
+    after(function(done) {
+        chai.request.agent(server).close();
+        done();
+    })
+
     test('POST to /api/check returns 200', function() {
         chai.request(server)
         .post('/api/check')
@@ -30,14 +36,60 @@ suite('Functional Tests', function () {
             .type('form')
             .send({
                 puzzle: validUnsolved,
-                coordinate: 'A1',
-                value: "1"
+                coordinate: "A2",
+                value: "9"
             })
             .end(function(err, res) {
-                assert.equal(res.status, 400);
-                assert.isArray(res.body);
-                assert.isFalse(res.body[0]);
+                assert.equal(err, null);
+                assert.equal(res.status, 200);
+                assert.isObject(res.body);
+                assert.isFalse(res.body.valid);
+                assert.isArray(res.body.conflict);
+                assert.equal(res.body.conflict[0], "column");
             });
-    })
+    });
+
+    test('POST to /api/check reports multiple placement conflict', function() {
+        chai.request(server)
+            .post('/api/check')
+            .type("form")
+            .send({
+                puzzle: validUnsolved,
+                coordinate: "A2",
+                value: "5"
+            })
+            .end(function(err, res) {
+                assert.equal(err, null);
+                assert.equal(res.status, 200);
+                assert.isObject(res.body);
+                assert.isFalse(res.body.valid);
+                assert.isArray(res.body.conflict);
+                assert.isTrue(res.body.conflict.length > 1);
+                assert.equal(res.body.conflict[0], "row");
+                assert.equal(res.body.conflict[1], "region");
+            })
+    });
+
+    test('POST to /api/check reports all placement conflict', function() {
+        chai.request(server)
+            .post('/api/check')
+            .type("form")
+            .send({
+                puzzle: validUnsolved,
+                coordinate: "E4",
+                value: "3"
+            })
+            .end(function(err, res) {
+                assert.equal(err, null);
+                assert.equal(res.status, 200);
+                assert.isObject(res.body);
+                assert.isFalse(res.body.valid);
+                assert.isArray(res.body.conflict);
+                assert.isTrue(res.body.conflict.length === 3);
+                assert.isTrue(res.body.conflict.includes("row"));
+                assert.isTrue(res.body.conflict.includes("column"));
+                assert.isTrue(res.body.conflict.includes("region"));
+            })
+    });
 });
 
